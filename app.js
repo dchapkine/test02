45f56cbcceaf77276f144cb7d81d914c858693f6
Dmitri Chapkine <dmitri.chapkine@gmail.com>
2015-11-05 04:48:21 +0100

    test

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var server = http.createServer(function (request, response) {
 });
 
 // Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(80);
+server.listen(80, "0.0.0.0");
 
 // Put a friendly message on the terminal
 console.log("Server running at http://127.0.0.1:80/");
